9b1a4d2f228f511a46c73b9b56b5743d2c3ab462
greengearx <g.p.s.frankhuizen@student.vu.nl>
2017-12-29 13:14:20 +0100

    Added first tests

---
+++ tests/test_deck.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from api import Deck
+
+
+class TestDeck(TestCase):
+
+    def test_generate(self):
+        d = Deck.generate()
+
+        stock = d.getState().count("Stock")
+        self.assertEqual(stock,10,"The value should be 10")
+
+        player1 = d.getState().count("P1Hand")
+        self.assertEqual(player1, 5,"The value should be 5")
+
+        player2 = d.getState().count("P2Hand")
+        self.assertEqual(player2, 5,"The value should be 5")
